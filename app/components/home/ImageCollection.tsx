2c7949717ac54f29df280118c180f0ae436f6e32
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-02-09 08:20:14 +0100

    .

---
+++ app/components/home/ImageCollection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const ImageCollection = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <img src="/img-collection-1.jpg" alt="" className="w-60  shadow-lg" />
+      <img src="/sunglassesOnBeach.png" alt="" className="w-60  shadow-lg" />
+      <img src="/sunglassesOnTowel.png" alt="" className="w-60  shadow-lg" />
+    </div>
+  );
+};
+
+export default ImageCollection;
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,11 @@ import Container from "./components/Container";
 
 import ProductsCard from "@/app/components/products/ProductsCard";
 import CategorySection from "./components/home/CategorySection";
-import ImageSlider from "./components/home/ImageSlider";
 
-import sunglassesOnBeach from "@/public/sunglassesOnBeach.png";
-import sunglassesOnTowel from "@/public/sunglassesOnTowel.png";
-import sunglassesOnTable from "@/public/sunglassesOnTable.png";
 import ImageSliderSection from "./components/home/ImageSliderSection";
 import NewsLetter from "./components/home/NewsLetter";
 import Link from "next/link";
-
-const imageSlides = [sunglassesOnBeach, sunglassesOnTable, sunglassesOnTowel];
+import ImageCollection from "./components/home/ImageCollection";
 
 export default async function Home() {
   const products = await getProductByBrand();
@@ -21,14 +16,14 @@ export default async function Home() {
     <>
       <div className="w-full h-screen sm:bg-[url('/small-hero.jpg')] bg-[url('/mobilehero2.jpg')] md:bg-[url('/medium-hero.jpg')] lg:bg-[url('/coolbg.jpg')] bg-cover">
         <div className="w-full flex justify-center sm:justify-start lg:ml-52 md:ml-28 sm:ml-20 h-full align-middle sm:items-center">
-          <div className="mt-20">
-            <h1 className="font-bold text-3xl ml-1">By </h1>
-            <h1 className="text-8xl font-bold">Dormo</h1>
+          <div className="mt-[60px]">
+            <h1 className="font-extrabold text-3xl ml-1 -mb-3">By </h1>
+            <h1 className="text-8xl  font-extrabold">Dormo</h1>
             <div className="h-[2px] w-full bg-black" />
 
             <h1 className="font-extrabold text-7xl mb-5 ml-1">New in</h1>
             <Link
-              href={""}
+              href={"/products?brand=Dormo"}
               className="border-[2px] hidden sm:block max-w-36 text-center text-nowrap border-black py-3 px-5 font-semibold text-lg mt-6 bg-stone-900 text-white rounded-xl hover:bg-slate-900/90"
             >
               Shop now
@@ -50,10 +45,9 @@ export default async function Home() {
       <div className="bg-gradient-to-r from-[#eaeae8] from-10% via-[#acaea1] via-50% to-[#7f8175] to-80% mt-24 w-full flex flex-col ">
         <CategorySection />
 
-        <div className="flex flex-col-reverse md:flex-row justify-center md:items-center  pb-20 sm:px-10 sm:py-8 md:py-20">
+        <div className="flex flex-col-reverse md:flex-row md:justify-between md:items-center  pb-20 sm:px-10 sm:py-8 md:py-20 md:px-40">
           <ImageSliderSection />
-
-          <ImageSlider imageUrls={imageSlides!} />
+          <ImageCollection />
         </div>
       </div>
       <NewsLetter />
