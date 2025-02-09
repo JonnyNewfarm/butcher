0f2bf443bf09d72ca9cd0ff1524bd0c85fea60c6
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-02-09 05:15:08 +0100

    .

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -6,15 +6,15 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
-  const [showInput, setShowInput] = useState("invisible");
+  const [showInput, setShowInput] = useState("hidden");
 
   const handleInputVisability = () => {
-    if (showInput === "invisible") {
-      return setShowInput("visable");
+    if (showInput === "hidden") {
+      return setShowInput("block");
     }
 
-    if (showInput === "visable") {
-      setShowInput("invisible");
+    if (showInput === "block") {
+      setShowInput("hidden");
     }
   };
 
@@ -34,7 +34,7 @@ const SearchBar = () => {
           id="search"
           placeholder="Explore"
           value={searchQuery}
-          className={`p-2 absolute sm:relative h-9 left-10 right-5 rounded-xl  z-50 sm:l sm:mb-[-7px]   sm:mr-6 bottom-1 border-stone-400  md:focus:outline-none focus:border-[0.5px] focus:border-stone-600 ${showInput}   `}
+          className={`p-2 absolute sm:relative h-9  left-10 right-5 rounded-xl  z-50 sm:l sm:mb-[-7px]   sm:mr-6 bottom-1 border-stone-400  md:focus:outline-none focus:border-[0.5px] focus:border-stone-600 ${showInput}   `}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </form>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import getProductByBrand from "@/actions/getProductByBrand";
 import Container from "./components/Container";
 
-import HeroSection from "./components/home/HeroSection";
 import ProductsCard from "@/app/components/products/ProductsCard";
 import CategorySection from "./components/home/CategorySection";
 import ImageSlider from "./components/home/ImageSlider";
@@ -11,6 +10,7 @@ import sunglassesOnTowel from "@/public/sunglassesOnTowel.png";
 import sunglassesOnTable from "@/public/sunglassesOnTable.png";
 import ImageSliderSection from "./components/home/ImageSliderSection";
 import NewsLetter from "./components/home/NewsLetter";
+import Link from "next/link";
 
 const imageSlides = [sunglassesOnBeach, sunglassesOnTable, sunglassesOnTowel];
 
@@ -19,7 +19,23 @@ export default async function Home() {
 
   return (
     <>
-      <div className="justify-center flex w-full h-screen bg-[url('/coolbg.jpg')] bg-cover"></div>
+      <div className="w-full h-screen sm:bg-[url('/small-hero.jpg')] bg-[url('/mobilehero2.jpg')] md:bg-[url('/medium-hero.jpg')] lg:bg-[url('/coolbg.jpg')] bg-cover">
+        <div className="w-full flex justify-center sm:justify-start lg:ml-52 md:ml-28 sm:ml-20 h-full align-middle sm:items-center">
+          <div className="mt-20">
+            <h1 className="font-bold text-3xl ml-1">By </h1>
+            <h1 className="text-8xl font-bold">Dormo</h1>
+            <div className="h-[2px] w-full bg-black" />
+
+            <h1 className="font-extrabold text-7xl mb-5 ml-1">New in</h1>
+            <Link
+              href={""}
+              className="border-[2px] hidden sm:block max-w-36 text-center text-nowrap border-black py-3 px-5 font-semibold text-lg mt-6 bg-stone-900 text-white rounded-xl hover:bg-slate-900/90"
+            >
+              Shop now
+            </Link>
+          </div>
+        </div>
+      </div>
 
       <Container>
         <div className="mt-24 border-t-[1px] bg-custom-color w-full rounded-xl">
