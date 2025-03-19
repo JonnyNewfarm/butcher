5ba2988faddc4f399b9499ec871f08eadd996fb4
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-03-19 04:23:42 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,14 @@ import Container from "./components/Container";
 
 import CategorySection from "./components/home/CategorySection";
 
-import ImageSliderSection from "./components/home/ImageSliderSection";
 import NewsLetter from "./components/home/NewsLetter";
 import Link from "next/link";
-import ImageCollection from "./components/home/ImageCollection";
 import ScrollSection from "./components/ScrollSection";
 import ProductCardContainer from "./components/home/ProductCardContainer";
 import ImgParallax from "./components/home/ImgParallax";
 import ParallaxContainer from "./components/home/ParallaxContainer";
 import HeroSection from "./components/home/HeroSection";
 import HeroContainer from "./components/home/HeroContainer";
-import ScrollText from "./components/home/ScrollText";
 
 export default async function Home() {
   const products = await getProductByBrand();
